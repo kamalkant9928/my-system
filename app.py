ae59c7db91b321e47fc219acd1cf24239d772608
KAMALKANT <KAMALKANT8000251291@gmail.com>
2026-03-28 17:17:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 st.title("📊 Simple ML Model (Pass/Fail Prediction)")
+st.title("KAMALKANT 📊 PRAJAPATI")
 
 # -----------------------------
 # Step 1: Train model on the fly
